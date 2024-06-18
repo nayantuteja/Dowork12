dbad3d3997afad03762bb09fc0c037ba1fb10f26
nayantuteja <141990913+nayantuteja@users.noreply.github.com>
2024-06-18 22:54:34 +0530

    Update AddTodo.jsx

--- src/app/components/AddTodo.jsx
+++ src/app/components/AddTodo.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import { useDispatch } from 'react-redux'
-import {addTodo,numberTodo} from '../features/todo/todoSlice'
+import {addTodo} from '../features/todo/todoSlice'
 
 function AddTodo() {
    
